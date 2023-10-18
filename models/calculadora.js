f6b3a09c6bf352e02d0a5361bfa6351ed56cc388
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2023-10-17 22:42:28 -0300

    feat: add status endpoint

--- models/calculadora.js
+++
@@ -1,8 +0,0 @@
-function somar(argumento1, argumento2) {
-  if (typeof argumento1 !== "number" || typeof argumento2 !== "number") {
-    return "erro";
-  }
-  return argumento1 + argumento2;
-}
-
-exports.somar = somar;
